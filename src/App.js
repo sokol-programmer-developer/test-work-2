0eac074cd3b764d5e780d7fb10218dea82aff3a7
sokol-programmer-developer <90709807+sokol-programmer-developer@users.noreply.github.com>
2021-12-03 16:23:04 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import { Routes, Route, } from 'react-router-dom';
+import Home from "./components/Home";
+import Article from "./components/Article";
 
-function App() {
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <Routes>
+             <Route path="/" element={<Home />} exact/>
+             <Route path="/Article" element={<Article />} exact/>
+          </Routes>
+      </div>
   );
 }
 
