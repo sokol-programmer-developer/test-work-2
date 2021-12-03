4b6df824116f2ed0b997ae2ca39f08bc11751f1f
sokol-programmer-developer <90709807+sokol-programmer-developer@users.noreply.github.com>
2021-12-03 16:29:53 +0300

    Update CarouselContent.jsx

--- src/components/CarouselContent.jsx
+++ src/components/CarouselContent.jsx
@@ -1,112 +1,193 @@
 import React from "react";
-import "./Style.css";
+import "../css/carousel.css";
 import Carousel from "./Carousel";
 
 import pic_basket from "./../assets/basketPic.svg"
+import {Link} from "react-router-dom";
 
 
 const CarouselContent = () => {
     return (
         <div className="carousel_section">
 
-            <Carousel  show={5} >
+            <h2 className="carousel_h2" > Часто задаваемые вопросы </h2>
 
-                <div  className='slider_section'>
+            <Carousel
+                show={4}
 
-                    <img src={ pic_basket } alt="pic" className="slider_pic" />
+            >
 
-                    <div className="slider_content">
-
-                        <div className="slider_text">
-
-                            <h2 className="slider_h2"> Slide - 1 </h2>
-                            <h3 className="slider_h3"> Lorem ipsum dolor sit amet, consectetur adipisicing. </h3>
-
-                        </div>
-
-                        <button className="slider_button">
-                            <h2 className="slider_button_h2"> Click Me </h2>
-                        </button>
-
-                    </div>
-                </div>
-                <div  className='slider_section'>
-
-                    <img src={ pic_basket } alt="pic" className="slider_pic" />
+                <div  className="slider_section">
 
                     <div className="slider_content">
 
                         <div className="slider_text">
 
-                            <h2 className="slider_h2"> Slide - 2 </h2>
-                            <h3 className="slider_h3"> Lorem ipsum dolor sit amet, consectetur adipisicing. </h3>
+                            <h2 className="slider_h2"> Где работает доставка? </h2>
+                            <h3 className="slider_h3"> Карта лояльности с вашим номером покупателя. <br/> Это карта лояльности с вашим. </h3>
 
                         </div>
-
-                        <button className="slider_button">
-                            <h2 className="slider_button_h2"> Click Me </h2>
-                        </button>
+                        <Link to="Article">
+                            <button className="slider_button">
+                                <h2 className="slider_button_h2"> Узнать </h2>
+                            </button>
+                        </Link>
 
                     </div>
-                </div>
-                <div  className='slider_section'>
 
                     <img src={ pic_basket } alt="pic" className="slider_pic" />
+
+                </div>
+                <div  className="slider_section">
 
                     <div className="slider_content">
 
                         <div className="slider_text">
 
-                            <h2 className="slider_h2"> Slide - 3 </h2>
-                            <h3 className="slider_h3"> Lorem ipsum dolor sit amet, consectetur adipisicing. </h3>
+                            <h2 className="slider_h2"> Где работает доставка? </h2>
+                            <h3 className="slider_h3"> Карта лояльности с вашим номером покупателя. <br/> Это карта лояльности с вашим. </h3>
+
 
                         </div>
 
-                        <button className="slider_button">
-                            <h2 className="slider_button_h2"> Click Me </h2>
-                        </button>
+                        <Link to="Article">
+                            <button className="slider_button">
+                                <h2 className="slider_button_h2"> Узнать </h2>
+                            </button>
+                        </Link>
 
                     </div>
-                </div>
-                <div  className='slider_section'>
 
                     <img src={ pic_basket } alt="pic" className="slider_pic" />
+
+                </div>
+                <div  className="slider_section">
 
                     <div className="slider_content">
 
                         <div className="slider_text">
 
-                            <h2 className="slider_h2"> Slide - 4 </h2>
-                            <h3 className="slider_h3"> Lorem ipsum dolor sit amet, consectetur adipisicing. </h3>
+                            <h2 className="slider_h2"> Где работает доставка? </h2>
+                            <h3 className="slider_h3"> Карта лояльности с вашим номером покупателя. <br/> Это карта лояльности с вашим. </h3>
 
                         </div>
 
-                        <button className="slider_button">
-                            <h2 className="slider_button_h2"> Click Me </h2>
-                        </button>
+                        <Link to="Article">
+                            <button className="slider_button">
+                                <h2 className="slider_button_h2"> Узнать </h2>
+                            </button>
+                        </Link>
 
                     </div>
-                </div>
-                <div  className='slider_section'>
 
                     <img src={ pic_basket } alt="pic" className="slider_pic" />
+
+                </div>
+                <div  className="slider_section">
 
                     <div className="slider_content">
 
                         <div className="slider_text">
 
-                            <h2 className="slider_h2"> Slide - 5 </h2>
-                            <h3 className="slider_h3"> Lorem ipsum dolor sit amet, consectetur adipisicing. </h3>
+                            <h2 className="slider_h2"> Где работает доставка? </h2>
+                            <h3 className="slider_h3"> Карта лояльности с вашим номером покупателя. <br/> Это карта лояльности с вашим. </h3>
 
                         </div>
 
-                        <button className="slider_button">
-                            <h2 className="slider_button_h2"> Click Me </h2>
-                        </button>
+                        <Link to="Article">
+                            <button className="slider_button">
+                                <h2 className="slider_button_h2"> Узнать </h2>
+                            </button>
+                        </Link>
 
                     </div>
-                </div>
 
+                    <img src={ pic_basket } alt="pic" className="slider_pic" />
+                </div>
+                <div  className="slider_section">
+
+                    <div className="slider_content">
+
+                        <div className="slider_text">
+
+                            <h2 className="slider_h2"> Где работает доставка? </h2>
+                            <h3 className="slider_h3"> Карта лояльности с вашим номером покупателя. <br/> Это карта лояльности с вашим. </h3>
+
+                        </div>
+
+                        <Link to="Article">
+                            <button className="slider_button">
+                                <h2 className="slider_button_h2"> Узнать </h2>
+                            </button>
+                        </Link>
+
+                    </div>
+
+                    <img src={ pic_basket } alt="pic" className="slider_pic" />
+                </div>
+                <div  className="slider_section">
+
+                    <div className="slider_content">
+
+                        <div className="slider_text">
+
+                            <h2 className="slider_h2"> Где работает доставка? </h2>
+                            <h3 className="slider_h3"> Карта лояльности с вашим номером покупателя. <br/> Это карта лояльности с вашим. </h3>
+
+                        </div>
+
+                        <Link to="Article">
+                            <button className="slider_button">
+                                <h2 className="slider_button_h2"> Узнать </h2>
+                            </button>
+                        </Link>
+
+                    </div>
+
+                    <img src={ pic_basket } alt="pic" className="slider_pic" />
+                </div>
+                <div  className="slider_section">
+
+                    <div className="slider_content">
+
+                        <div className="slider_text">
+
+                            <h2 className="slider_h2"> Где работает доставка? </h2>
+                            <h3 className="slider_h3"> Карта лояльности с вашим номером покупателя. <br/> Это карта лояльности с вашим. </h3>
+
+                        </div>
+
+                        <Link to="Article">
+                            <button className="slider_button">
+                                <h2 className="slider_button_h2"> Узнать </h2>
+                            </button>
+                        </Link>
+
+                    </div>
+
+                    <img src={ pic_basket } alt="pic" className="slider_pic" />
+                </div>
+                <div  className="slider_section">
+
+                    <div className="slider_content">
+
+                        <div className="slider_text">
+
+                            <h2 className="slider_h2"> Где работает доставка? </h2>
+                            <h3 className="slider_h3"> Карта лояльности с вашим номером покупателя. <br/> Это карта лояльности с вашим. </h3>
+
+                        </div>
+
+                        <Link to="Article">
+                            <button className="slider_button">
+                                <h2 className="slider_button_h2"> Узнать </h2>
+                            </button>
+                        </Link>
+
+                    </div>
+
+                    <img src={ pic_basket } alt="pic" className="slider_pic" />
+                </div>
 
             </Carousel>
         </div>
@@ -115,4 +196,3 @@ const CarouselContent = () => {
 }
 
 export default CarouselContent;
-
